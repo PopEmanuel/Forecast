bdad0a4b373fc836135730a5dba7a687aef7dd8b
Pop Emanuel <56311915+PopEmanuel@users.noreply.github.com>
2023-10-04 12:54:56 +0300

    Add exception handler

---
+++ forecast/src/main/java/com/pop/forecast/handler/ApiExceptionHandler.java
@@ -0,0 +1,24 @@
+package com.pop.forecast.handler;
+
+import com.pop.forecast.exception.ApiException;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
+
+@ControllerAdvice
+@Slf4j
+public class ApiExceptionHandler {
+
+    @ExceptionHandler(value = {RuntimeException.class})
+    public ResponseEntity<Object> handleJobFailureException(RuntimeException e) {
+        ApiException apiException = new ApiException(e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR, 500, ZonedDateTime.now(ZoneId.of("Z")));
+        log.error(e.getMessage(), e);
+        return new ResponseEntity<>(apiException, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
+}
